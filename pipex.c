5b8d4476d61333d4311089ae595c208bc36e7170
Rachida Moujan <rmoujan@student.1337.ma>
2022-03-23 16:56:01 +0100

    trying to understand functions

--- pipex.c
+++ pipex.c
@@ -6,21 +6,40 @@
 /*   By: rmoujan < rmoujan@student.1337.ma>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/16 12:31:28 by rmoujan           #+#    #+#             */
-/*   Updated: 2022/03/23 15:59:35 by rmoujan          ###   ########.fr       */
+/*   Updated: 2022/03/23 16:54:31 by rmoujan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-
+void checks_errors(void)
+{
+    write(1, "ERROR MANAGEMENT \n", 18);
+    exit(1);
+}        
+void test_files(char *argv[])
+{
+    if (access(argv[1], F_OK) == -1 || access(argv[1], R_OK | W_OK | X_OK) == -1)
+    {
+        write(1, "ERROR OCCURED WHEN CHECKING FILES \n", 35);
+        exit(1);
+    }
+    // else
+    // {
+    //     printf("there is no error \n");
+    // }
+    // int test, test2;
+    // test = access(argv[1], F_OK);
+    // printf("test == %d\n", test);
+    // test2 = access(argv[1], R_OK | W_OK | X_OK);
+    // printf("tes2 == %d\n", test2);
+}
 
 int main(int argc, char *argv[])
 {
     if (argc != 5)
-    {
-        write(1, "ERROR MANAGEMENT \n", 18);
-        exit(1);
-        
-    }
+        checks_errors();
+    test_files(argv);
     
+    return (0);
 }
--- pipex.h
+++ pipex.h
@@ -6,7 +6,7 @@
 /*   By: rmoujan < rmoujan@student.1337.ma>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/16 12:32:06 by rmoujan           #+#    #+#             */
-/*   Updated: 2022/03/23 12:44:03 by rmoujan          ###   ########.fr       */
+/*   Updated: 2022/03/23 16:28:52 by rmoujan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,5 +18,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+void checks_errors(void);
+void test_files(char *argv[]);
 
 #endif
