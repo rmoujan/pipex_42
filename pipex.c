2b7da925816469fada292140b60c52f089f79c6b
Rachida Moujan <rmoujan@student.1337.ma>
2022-04-07 12:11:34 +0000

    starting the second prg and connect them

--- pipex.c
+++ pipex.c
@@ -6,7 +6,7 @@
 /*   By: rmoujan < rmoujan@student.1337.ma>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/16 12:31:28 by rmoujan           #+#    #+#             */
-/*   Updated: 2022/04/05 16:46:10 by rmoujan          ###   ########.fr       */
+/*   Updated: 2022/04/07 12:10:37 by rmoujan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,14 +45,14 @@ void test_files(char *argv[])
     // printf("tes2 == %d\n", test2);
 }
 
-//calcul how many lines we have in args for the first exceve
-// ./pipex infile "ls -l" "wc -l" outfile
-void	cmd_parameters(char **argv, t_cmd *cmd1)
+//bring the cmd and his parameters  :
+//must calcul the numbers of cmd and his parameters (to allocate the tab cmd):
+void	cmd_parameters(char **argv, t_arg *prg)
 {
-	cmd1->name = ft_split(argv[2], ' ');
-	cmd1->lines = 0;
-	while (cmd1->name[cmd1->lines])
-		cmd1->lines++;
+	prg->cmd.name = ft_split(argv[3], ' ');
+	prg->cmd.lines = 0;
+	while (prg->cmd.name[prg->cmd.lines])
+		prg->cmd.lines++;
 }
 
 void ft_exit()
@@ -62,101 +62,92 @@ void ft_exit()
 }
 
 //fill the args with the cmd and her parameters !!!
-void creating_args(char ***args, t_cmd cmd1)
+void creating_args(t_arg *prg)
 {
 	int i;
 	int j;
 
 	i = 1;
 	j = 0;
-	while (cmd1.name[j])
+    //prg->args[0] = "/usr/bin";
+	while (prg->cmd.name[j])
 	{
-		(*args)[i] = cmd1.name[j];
+        prg->args[i] = prg->cmd.name[j];
 		i++;
 		j++;
 	}
-	(*args)[i] = NULL;
+    prg->args[i] = NULL;
 }
 
 //should concatene each path with  "/commande" 
-char **concat_pathwithcmd(char **path, t_cmd cmd1)
+void concat_pathwithcmd(t_arg *prg)
 {
 	int i;
 
 	i = 0;
-	while (path[i])
+	while (prg->path[i])
 	{
-		path[i] = ft_strjoin(path[i], "/\0");
-		path[i] = ft_strjoin(path[i], cmd1.name[0]);
+		prg->path[i] = ft_strjoin(prg->path[i], "/\0");
+		prg->path[i] = ft_strjoin(prg->path[i], prg->cmd.name[0]);
 		i++;
 	}
-	return (path);
+	//return (prg->path[i]);
 }
 
-// void	getting_paths(char **envp, t_arg *prg)
-// {
-// 	int	i;
+void	getting_paths(char *const envp[], t_arg *prg)
+{
+	int	i;
 
-// 	i = 0;
-// 	while (envp[i])
-//     {
-//         if (ft_strncmp(envp[i], "PATH", 4) == 0)
-//         {
-//             prg->path = ft_split(envp[i], ':');
-//             prg->path[0] = ft_strtrim(prg->path[0], "PATH=");
-//             break;
-//         }
-//         i++;
-//     }
-// }
+	i = 0;
+	while (envp[i])
+    {
+        if (ft_strncmp(envp[i], "PATH", 4) == 0)
+        {
+            prg->path = ft_split(envp[i], ':');
+            prg->path[0] = ft_strtrim(prg->path[0], "PATH=");
+            break;
+        }
+        i++;
+    }
+}
 
 int main(int argc, char *argv[], char *const envp[])
 {
     int		id;
     int		i;
     int		flag;
-	int		size;
-    char	**path = NULL;
-	t_cmd	cmd1;
-	char	**args1;
-	//int		f[2];
-	//t_arg	prg1;
-	//t_arg	prg2;
+	int		pi[2];
+	t_arg	*prg1;
+	t_arg	*prg2;
 	
-   checks_errors(argc);
-    test_files(argv);
-    //bringing the var PATH from envp :
+    //checks_errors(argc);
+    //test_files(argv);
     i = 0;
     flag = 0;
-	//this part is worked well
-    while (envp[i] != NULL)
-    {
-        if (ft_strncmp(envp[i], "PATH", 4) == 0)
-        {
-            path = ft_split(envp[i], ':');
-            path[0] = ft_strtrim(path[0], "PATH=");
-            break;
-        }
-        i++;
-    }
-	//later :: 
-	//prg1 = malloc(sizeof(t_arg));
-	//getting_paths(envp, &prg1);
-	
-    // //fill the cmd1 with the path ,cmd and his parameters :
-	// //first of all must calcul how many args we have , for allocating the tab cmd1:
-	cmd_parameters(argv, &cmd1);
-	size = cmd1.lines + 2;
-	args1 = (char **) malloc(sizeof(char *) * (size));
-	if (args1 == NULL)
+	prg1 =(t_arg *) malloc(sizeof(t_arg));
+	getting_paths(envp, prg1);//WORKED
+	cmd_parameters(argv, prg1);
+	prg1->args = (char **) malloc(sizeof(char *) * (prg1->cmd.lines + 2));
+	if (prg1->args == NULL)
 		ft_exit();
-	creating_args(&args1, cmd1);
-	path = concat_pathwithcmd(path, cmd1);
-		
+	creating_args(prg1);
+	concat_pathwithcmd(prg1);
+    //preparing the 2 cmd :
+    prg2 =(t_arg *) malloc(sizeof(t_arg));
+	getting_paths(envp, prg2);//WORKED
+	cmd_parameters(argv, prg2);
+	prg2->args = (char **) malloc(sizeof(char *) * (prg2->cmd.lines + 2));
+	if (prg2->args == NULL)
+		ft_exit();
+	creating_args(prg2);
+	concat_pathwithcmd(prg2);
+        
 	int fd = open(argv[1], O_RDONLY);
+    int fd2 = open(argv[4], O_RDWR);
 	if (fd == -1)
 		ft_exit();
-	//int fd = file_into_str(argv[1]);
+    if (pipe(pi) < 0)
+        exit(1);
     id = fork();
 	i = 0;
     if (id < 0)
@@ -164,25 +155,30 @@ int main(int argc, char *argv[], char *const envp[])
     //child process :
     else if (id == 0)
     {
-        //should do here the first execeve function to execute the cmd1
-        //checking is the command exits in some of the paths by using execeve :
+        //redirect the input to file
 		if (dup2(fd, 0) == -1)
 			perror("dup2");
 		close(fd);
-        while (path[i])
+        //for the pipe
+        close(pi[0]);
+        if (dup2(pi[1], 1) == -1)
+            perror("dup2");
+        close(pi[1]);
+        //should do here the first execeve function to execute the cmd1
+        //checking is the command exits in some of the paths by using execeve :
+        while (prg1->path[i])
         {
 			flag = 0;
-			args1[0] = path[i];
-            if (execve(path[i], args1 + 1, NULL) == -1)
+			prg1->args[0] = prg1->path[i];
+            if (execve(prg1->path[i], prg1->args + 1, NULL) == -1)
                 flag = 1;
             i++;
         }
 		//if the command does not exists will appear this msg : 
 		if (flag == 1)
 		{
-			printf("==============");
 			perror("execve");
-			printf("insid if when execve failed !!!!! \n");
+            ft_exit();
 		}
     }
     //parent process :
@@ -191,6 +187,33 @@ int main(int argc, char *argv[], char *const envp[])
         //will wait until the child process finish for bringing the data from child process!!
         //should do here the second execeve function to execute the cmd2
         wait(NULL);
+        close(pi[1]);
+        //redirect the THE pipe to input
+		if (dup2(pi[0], 0) == -1)
+			perror("dup2");
+		close(pi[0]);
+        close(pi[1]);
+        //rdirect the output to file
+        if (dup2(fd2, 1) == -1)
+        {
+            printf("error");
+            perror("dup2");
+        }
+        //close(fd2);
+        while (prg2->path[i])
+        {
+			flag = 0;
+			prg2->args[0] = prg2->path[i];
+            if (execve(prg2->path[i], prg2->args + 1, NULL) == -1)
+                flag = 1;
+            i++;
+        }
+		//if the command does not exists will appear this msg : 
+		if (flag == 1)
+		{
+			perror("execve");
+            ft_exit();
+		}
         printf("****** PARENT PROCESS ******\n");
     }
 	
