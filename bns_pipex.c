0f7ed8179fcb53feaf408771ad63e8e087c8f953
Rachida Moujan <rmoujan@student.1337.ma>
2022-04-13 14:37:34 +0000

    bonus_part

--- bns_pipex.c
+++ bns_pipex.c
@@ -6,15 +6,58 @@
 /*   By: rmoujan < rmoujan@student.1337.ma>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/08 10:20:25 by rmoujan           #+#    #+#             */
-/*   Updated: 2022/04/08 15:22:46 by rmoujan          ###   ########.fr       */
+/*   Updated: 2022/04/13 12:09:23 by rmoujan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
 //handle multiple pipes :
-
-int main(int args, char *argv[])
+void	ft_error(char *str)
 {
+	perror(str);
+	exit(1);
+}
+int main(int argc, char *argv[])
+{
+	int i;
+	int pid;
+	int pid2[argc - 5];
+    int fd[argc - 4][2];
     
+	i = 0; 
+	//creation of the pipe:
+    while (i < (argc - 4))
+    {
+        if (pipe(fd[i]) == -1)
+            ft_error("pipe");
+		//printf("test\n");
+		i++;
+	}
+	i = 0;
+	pid = fork();
+	if (pid < 0)
+		 ft_error("fork");
+	else if (pid == 0)
+	{
+		//first child process :
+		printf("first child process \n");
+		
+	}
+	else
+	{
+		//parent process :
+		while (i < (argc - 5))
+		{
+			pid2[i] = fork();
+			if (pid2[i] < 0)
+				ft_error("fork");
+			if (pid2[i] == 0)
+			{
+				printf("process number %d\n", i);		
+			}
+			i++;
+		}
+	}
+	return (0);
 }
---
+++ tests/multiplr_fork.c
@@ -0,0 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   multiplr_fork.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rmoujan < rmoujan@student.1337.ma>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/13 13:12:31 by rmoujan           #+#    #+#             */
+/*   Updated: 2022/04/13 14:28:19 by rmoujan          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <unistd.h>
+#include "../pipex.h"
+
+int main(int argc, char *argv[])
+{
+    //creating multiple processes :
+    int pid;
+    int pi[3];
+    int i = 0;
+
+    for(int i=0;i<6;i++) // loop will run n times (n=5)
+    {
+        if(fork() == 0)
+        {
+            printf("[son] pid %d from [parent] pid %d\n",getpid(),getppid());
+            exit(0);
+        }
+    }
+    for(int i=0;i<2;i++) // loop will run n times (n=5)
+        wait(NULL);
+
+    // pid = fork();
+
+    // if (pid < 0)
+    //     perror("fork");
+    // else if(pid == 0)
+    // {
+    //     //first child process :
+    //     printf("[son] pid %d from [parent] pid %d\n", getpid(), getppid());
+    // }
+    // else
+    // {
+    //     while (i < 3)
+    //     {
+    //         pi[i] = fork();
+    //         if (pi[i] == 0)
+    //             printf("[son] pid %d from [parent] pid %d\n", getpid(), getppid());
+    //         i++;
+    //     }
+    //     // printf("parent process %d\n", getppid());
+    // }
+}
