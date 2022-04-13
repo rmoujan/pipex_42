b6b4316f18e34f27cc860b8fee917858ccb0e8a8
Rachida Moujan <rmoujan@student.1337.ma>
2022-04-13 18:26:08 +0000

    starting bonus

--- bns_pipex.c
+++ bns_pipex.c
@@ -6,7 +6,7 @@
 /*   By: rmoujan < rmoujan@student.1337.ma>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/08 10:20:25 by rmoujan           #+#    #+#             */
-/*   Updated: 2022/04/13 14:53:44 by rmoujan          ###   ########.fr       */
+/*   Updated: 2022/04/13 18:25:36 by rmoujan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@ void	ft_error(char *str)
 	perror(str);
 	exit(1);
 }
+
 int main(int argc, char *argv[])
 {
 	int i;
@@ -30,32 +31,107 @@ int main(int argc, char *argv[])
     {
         if (pipe(fd[i]) == -1)
             ft_error("pipe");
-		//printf("test\n");
-		i++;
-	}
-	//starting forking :
-	i = 0;
-	while (i < (argc - 3))
-	{
-		if (pid[i] == fork())
-			//ft_error("fork");
+		// printf("test\n");
 		i++;
 	}
 	i = 0;
+	//creation processes :
 	while (i < (argc - 3))
 	{
-		if (pid[i] == 0)
-			{
-				//first process
-				printf("first process \n");
-			}
-		else
+		pid[i] = fork();
+		if(pid[i] < 0)
 		{
-			waitpid(pid[i], NULL, 0);
-			printf("parent process \n");
+			ft_error("fork");
+			// printf("pid[%d] == %d\n", i, pid[i]);
+			exit(0);
+			// printf("test \n");
 		}
-	i++;
+		// printf("test \n");
+		
+		i++;
 	}
+	i = 0;
+	while (i < (argc - 3 ))
+	{
+		//beginnen :
+		if (pid[i] == 0)
+		{
+			printf("child process number %d \n", i);
+		}
+		else
+		{int j = 0;
+			
+			while (j < (argc - 3))
+			{
+			waitpid(pid[j],NULL, 0);
+			printf("parent process number %d\n", j);
+			j++;
+			}
+
+		}
+		i++;
+	}
+	// for(int i=0;i<argc - 3;i++) // loop will run n times (n=5)
+    // {
+    //     if(fork() == 0)
+    //     {
+    //         printf("[son] pid %d from [parent] pid %d\n",getpid(),getppid());
+    //         exit(0);
+    //     }
+    // }
+    // for(int i=0;i<3;i++) // loop will run n times (n=5)
+    // wait(NULL);
+	// //starting forking:
+	// i = 0;
+	// pid[i] = fork();
+	
+	// if (pid[i++] == 0)
+	// {
+	// 	//first child process 
+	// }
+	// else {
+	// 	while (i < (argc - 4))
+	// 	{
+	// 		pid[i] = fork();
+	// 		if (pid[i++] == 0)
+	// 		{
+	// 			//second child :
+	// 		}
+	// 		else
+	// 		{
+					
+	// 		}
+	// 	}
+	// }
+	// i = 0;
+	// while (i < (argc - 3))
+	// {
+	// 	pid[i] = fork(); 
+	// 	if (pid[i] < 0)
+	// 		ft_error("fork");
+	// 	else if (pid[i] == 0)
+	// 	{
+	// 		//first process
+	// 		printf("first process number %d\n", i);
+	// 	}
+	// 	i++;
+	// }
+	
+	// i = 0;
+	// while (i < (argc - 3))
+	// {
+	// 	if (pid[i] == 0)
+	// 	{
+	// 		//first process
+	// 		printf("first process number %d\n", i);
+	// 	}
+	// 	// else
+	// 	// {
+	// 	// 	waitpid(pid[i], NULL, 0);
+	// 	// 	printf("parent process \n");
+	// 	// }
+	// i++;
+	// }
 	
 	// i = 0;
 	// pid = fork();
