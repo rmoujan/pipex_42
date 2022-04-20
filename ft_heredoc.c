f2d700fe0dc6fbef403e10c655f4832fadaed97a
Rachida Moujan <rmoujan@student.1337.ma>
2022-04-20 02:44:58 +0000

    bns part .. here doc still does not work

--- ft_heredoc.c
+++ ft_heredoc.c
@@ -6,7 +6,7 @@
 /*   By: rmoujan < rmoujan@student.1337.ma>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/20 00:28:03 by rmoujan           #+#    #+#             */
-/*   Updated: 2022/04/20 02:07:59 by rmoujan          ###   ########.fr       */
+/*   Updated: 2022/04/20 02:42:01 by rmoujan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,6 +36,27 @@ void ft_heredoc(int argc, char *argv[], char *const envp[])
 		ft_exit();
     //when the input is from HEREDOC :
     //starting herdoc:
-	get_input_herdoc(id, argv[2]);
+	// get_input_herdoc(id, argv[2]);
+	char *str;
+    if (pipe(id.pidoc[0]) == -1)
+		ft_error("pipe");
+    write(0,"heredoc>", 8);
+	str = get_next_line(0);
+	str = ft_strtrim(str, "\n");
+	while (strcmp(str, argv[2]) != 0)
+	{
+		str = ft_strjoin(str, "\n");
+		write(0,"heredoc>", 8);
+		write(id.pidoc[0][1], str, ft_strlen(str));
+		free(str);
+		str = get_next_line(0);
+		str = ft_strtrim(str, "\n");	
+	}
+	close(id.pidoc[0][1]);
+	read(id.pidoc[0][0], str, 1000);
+	printf("***%s***\n", str);
 	forking_heredoc(id, prg1, prg2, envp);
 }
+
+///NOTES : FASH KANSIFT PIPE L FCT B7ALI KAY LOSE DATA LI MESTORYA FIH
+///NOTES2: HERE DOC IS NOT WORKING CUZ THE FIRST PIPE DON'T SAVE THE DATA WELL
