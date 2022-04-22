37a8a21c9596f24c0c090d9597c144b7de068869
Rachida Moujan <rmoujan@student.1337.ma>
2022-04-22 22:27:56 +0000

    all good but still must handle all errors

--- operations2.c
+++ operations2.c
@@ -6,36 +6,38 @@
 /*   By: rmoujan < rmoujan@student.1337.ma>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/20 02:04:41 by rmoujan           #+#    #+#             */
-/*   Updated: 2022/04/22 00:31:18 by rmoujan          ###   ########.fr       */
+/*   Updated: 2022/04/22 22:21:18 by rmoujan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 #include "libft/libft.h"
-// int	check_script(t_arg *prg)
-// {
-// 	int	i;
-// 	int flag;
 
-// 	i = 0;
-// 	flag = 0;
-// 	while (prg->path[i])
-// 	{
-// 		if (access(prg->path[i], F_OK) != -1)
-// 		{
-// 			flag = 1;
-// 		}
-// 		i++;
-// 	}
-// 	return (flag);
-// }
+int	check_script(t_arg *prg)
+{
+	int	i;
+	int flag;
+
+	i = 0;
+	flag = 0;
+	while (prg->cmd[i])
+	{
+		if (access(prg->path[i], F_OK) != -1)
+		{
+			flag = 1;
+		}
+		i++;
+	}
+	return (flag);
+}
+
 void	concaten_pathscmd(t_arg *prg, char *argv)
 {
 	int	i;
 
 	i = 0;
 	prg->cmd = ft_split(argv, ' ');
-	// check_script(prg);
+	check_script(prg);
 	ft_check(prg);
 	while (prg->path[i])
 	{
